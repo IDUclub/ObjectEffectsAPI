b74b0bd7ada5d853763d088129e625d254c66b5b
Leon <turkov.leon-2@yandex.ru>
2025-06-25 18:35:58 +0300

    fix(main): - log names updated

--- app/main.py
+++ app/main.py
@@ -37,7 +37,7 @@ async def get_logs():
 
     try:
         return FileResponse(
-            f"{config.get('LOG_FILE')}.log",
+            ".log",
             media_type='application/octet-stream',
             filename=f"ObjectEffects.log",
         )
@@ -45,14 +45,14 @@ async def get_logs():
         raise http_exception(
             status_code=404,
             msg="Log file not found",
-            _input={"lof_file_name": f".log"},
+            _input={"lof_file_name": ".log"},
             _detail={"error": e.__str__()}
         )
     except Exception as e:
         raise http_exception(
             status_code=500,
             msg="Internal server error during reading logs",
-            _input={"lof_file_name": f"{config.get('LOG_FILE')}.log"},
+            _input={"log_file_name": ".log"},
             _detail={"error": e.__str__()}
         )
 
